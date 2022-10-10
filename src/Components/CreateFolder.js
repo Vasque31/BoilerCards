c09f52b25dab75dcb43d049a3489d137d15c8b53
Neihl Wang <wang4919@purdue.edu>
2022-10-09 21:00:27 -0400

    Updated CreateFolder.js
    some changes did not appear upon first commit

---
+++ src/Components/CreateFolder.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./CreateFolder.css";
+
+function CreateFolder() {
+
+    const handleCreation = (event) => {
+        event.preventDefault();
+        
+    }
+    return (
+        <div className='CreateFolder'>
+            <h1> Creating a Folder </h1>
+            <div>
+                <form action = "/url"> 
+                    <label for = "folderName">Name: </label>
+                    <input type = "text" id = "folderName" name = "folderName"> <br></br> </input>
+                    <input type = "submit" value = "Submit"></input>
+                    asdf
+
+                </form>
+
+
+            </div>
+
+        </div>
+  
+
+    );
+}
+export default CreateFolder;
