9d2dc143a6ab3189d6097d344048e49c9388fe5e
Vasque31 <vasque31@purdue.edu>
2022-10-12 15:11:17 -0400

    Axois Client to server connect
    client server connect axios

--- client/src/Components/signInPage.js
+++ client/src/Components/signInPage.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+
 import "./signInPage.css";
 import mylogo from "../images/PurdueTrain.png";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 //Use states for Sign In
 
 const errors = {
@@ -13,7 +15,8 @@ function SignInPage() {
     const [errorMessages, setErrorMessages] = useState({});
     const [isSubmitted, setIsSubmitted] = useState(false);
     const navigate = useNavigate();
-
+    const [password, setPassword] = useState();
+    const [username, setUsername] = useState();
     const handleSignUp = (event) => {
         //prevents page reload
         event.preventDefault();
@@ -24,9 +27,15 @@ function SignInPage() {
     const handleSignIn = (event) => {
         //prevents page reload
         event.preventDefault();
-        
+
+        const logginInfo = {
+            username: username,
+            password: password
+        }
         //Call to backend to check validity
         //if good link to homepage with the persons info
+        axios.get('http://localhost:3001/signin', logginInfo)
+        .then(response => console.log(response.data))
         navigate("/HomePage");
     };
     return (
@@ -35,11 +44,11 @@ function SignInPage() {
                 <img alt = "Logo" className = "photo" src= {mylogo}/>
                 <div className = "input-container">
                     <label>Username</label>
-                    <input type="text" name="uname" required />
+                    <input type="text" name="username" value={username} onChange={(event) => setUsername(event.target.value)} required />
                 </div>
                 <div className="input-container">
                     <label>Password </label>
-                    <input type="password" name="pass" required />
+                    <input type="password" name="password" value={password} onChange={(event) => setPassword(event.target.value)} required />
                 </div>
                 
                 <div className="button-container">
