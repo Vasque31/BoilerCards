fa2ea4caa7b099465fd5819383f8bd57c39633f9
Neihl Wang <wang4919@purdue.edu>
2022-10-13 18:06:37 -0400

    updated viewflashcard.js

--- client/src/Components/ViewFlashcard.js
+++ client/src/Components/ViewFlashcard.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import "./ViewFlashcard.css";
 import Carousel from 'react-bootstrap/Carousel';
-import background from '../images/PurdueTrain.png';
+import background from '../images/bk.png';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
+
 function ViewFlashcard() {
 
     const [index, setIndex] = useState(0);
@@ -12,30 +17,66 @@ function ViewFlashcard() {
 
 
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
+        <div style={{display: 'block', backgroundColor: 'darkgray', width: '100%'}}>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
             
-            <Carousel.Item>
-                <img
-                    className="background-1"
-                    src={background}
-                    alt="First slide"
-                />
-                <Carousel.Caption >
-                    <h1 styling={{fontSize: "5rem", color:"green", textAlign:"center"}}>Front Side of Flashcard</h1>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="background-2"
-                    src={background}
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                    <h2>Back Side of Flashcard</h2>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+                <Carousel.Item>
+                    <img
+                        className="background-1"
+                        src={background}
+                        alt="First slide"
+                        width="100%"
+                        height="500px"
+                    />
+                    
+                    <Carousel.Caption >
+                        <h3 styling={{fontSize: "5rem", color:"green", textAlign:"center"}}>Front Side of Flashcard</h3>
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="background-2"
+                        src={background}
+                        alt="Second slide"
+                        width="100%"
+                        height="500px"
+                    />
+                    <Carousel.Caption>
+                        <h2>Back Side of Flashcard</h2>
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            <div style={{backgroundColor: 'darkgray', width: '100%', height:'70%'}}>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Flashcard</th>
+                            <th>Front</th>
+                            <th>Back</th>
+                            <th>Edit or Delete</th>
+                        </tr>
+                        <tr>
+                            <th>Testing</th>
+                            <th>Sample front</th>
+                            <th>Sample Back side</th>
+                            <th>
+                                <ButtonGroup aria-label="Edit/Delete">
+                                    <Button variant="primary"> Edit </Button>
+                                    <Button variant="primary"> Delete </Button>
+                                </ButtonGroup>
+                            </th>
+                        </tr>
+                    </thead>
+
+                </Table>
+            </div>
+            
+        </div>
+
     );
+
 
 
 }
