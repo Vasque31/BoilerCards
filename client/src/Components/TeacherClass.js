5e766b728a539660b37bad3ef99cbedf14f62915
Vasque31 <vasque31@purdue.edu>
2022-12-02 13:23:37 -0500

    Update TeacherClass.js

--- client/src/Components/TeacherClass.js
+++ client/src/Components/TeacherClass.js
@@ -1,41 +1,36 @@
 import React, { useEffect, useState } from "react";
 import "./TeacherClass.css";
-import "./Header.css";
-import Button from "react-bootstrap/Button";
+import "./Header.css"
+import Button from 'react-bootstrap/Button';
 import CloseButton from "react-bootstrap/esm/CloseButton";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import TeacherHeader from "./TeacherHeader";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 import { libstorage } from "./signInPage";
-import { useCookies } from "react-cookie";
-import { getCookie, setCookie } from "react-use-cookie";
-import cookie from "react-cookies";
+import { useCookies } from 'react-cookie';
+import { getCookie, setCookie } from 'react-use-cookie';
+import cookie from 'react-cookies'
 import axios from "axios";
 
 import saveicon from "../images/saveicon.png";
 
-function TeacherClass() {
-  {
-    /*Create FlashCard Set Handlers*/
-  }
-  const fileReader = new FileReader();
-  const navigate = useNavigate();
-  const [show, setShow] = useState(false);
-  const [name, setName] = useState("");
-  const [inputList, setinputList] = useState([
-    { front: "", back: "", drate: "3", img: "" },
-  ]);
-  const [library, setLibrary] = useState(
-    JSON.parse(localStorage.getItem("class"))
-  );
-  const [dest, setDest] = useState(getCookie("classCode"));
-  const [showSaved, setShowSaved] = useState(false);
 
-  useEffect(() => {
+function TeacherClass() {
+   {/*Create FlashCard Set Handlers*/}
+   const fileReader = new FileReader();
+   const navigate = useNavigate();
+   const [show, setShow] = useState(false);
+   const [name, setName] = useState("");
+   const [inputList, setinputList] = useState([{front:'', back:'', drate: '3', img:''}]);
+   const [library, setLibrary] = useState(JSON.parse(localStorage.getItem('class')));
+   const [dest, setDest] = useState(getCookie('classCode'));
+   const [showSaved, setShowSaved] = useState(false);
+
+   useEffect(() => {
     const getLibrary = async () => {
       let res = await axios.post("http://localhost:3001/class", {
-        classCode: getCookie("classCode"),
+        classCode: getCookie('classCode'),
       });
       console.log(res.data);
       setLibrary(res.data);
@@ -43,122 +38,110 @@ function TeacherClass() {
     };
     getLibrary();
   }, []);
-  /** handlers **/
+     /** handlers **/
 
-  const handleSeeStudentGrade = async (uName) => {
-    console.log("selecting student");
-    console.log(uName);
-    setCookie("selectedStudent", uName);
-    console.log("cookie");
-    console.log(getCookie("selectedStudent"));
-    let res = await axios.post("http://localhost:3001/getScoreList", {
-      setID: JSON.parse(localStorage.getItem("flashcards")).flashcardset._id,
-      classCode: getCookie("classCode"),
-    });
-    localStorage.setItem("studentList", JSON.stringify(res.data));
-    //var studentList = res.data;
-    navigate("/studentgrade");
-  };
+    const handleSeeStudentGrade = (uName) => {
+        console.log("selecting student")
+        console.log(uName);
+        setCookie("selectedStudent", uName);
+        console.log(getCookie('selectedStudent'));
+        navigate("/studentgrade");
+    }
 
-  const handleShowSaved = () => {
-    setShowSaved(true);
-  };
-  const handleCloseSaved = () => {
-    setShowSaved(false);
-    handlerefresh();
-  };
+     const handleShowSaved = () => { setShowSaved(true);}
+     const handleCloseSaved = () => { 
+         setShowSaved(false);
+         handlerefresh();
+     }  
 
-  const handleFlashcardSetClick = async (id) => {
+   const handleFlashcardSetClick = async(id) => {
     console.log(id);
     let res = await axios.post("http://localhost:3001/flsahcardset", {
-      setid: id,
+        setid:id 
     });
-    localStorage.setItem("flashcards", JSON.stringify(res.data));
+    localStorage.setItem('flashcards', JSON.stringify(res.data));
     console.log(res.data);
-    navigate("/flashcard");
-  };
-  const handleaddmore = () => {
-    setinputList([...inputList, { front: "", back: "", drate: "3", img: "" }]);
-  };
-  const handleinputchange = (e, index) => {
-    const { name, value } = e.target;
-    const list = [...inputList];
-    list[index][name] = value;
-    setinputList(list);
-    console.log(inputList);
-  };
-  const handleCreateFlashCardSet = async (event) => {
-    event.preventDefault();
-
-    const flashcardInfo = {
-      inputList: inputList,
-      name: name,
-      classCode: getCookie("classCode"),
-    };
-    console.log(flashcardInfo);
-    let res = await axios.post("http://localhost:3001/createTeacherSet", {
-      inputList: flashcardInfo.inputList,
-      name: flashcardInfo.name,
-      classCode: flashcardInfo.classCode,
-    });
-
-    if (res.data === true) {
-      handleShowSaved();
+    navigate('/flashcard');
+   }
+   const handleaddmore = () => {
+    setinputList([...inputList, {front:'', back:'', drate:'3', img: ''}]);
     }
-    handleClose();
-    handlerefresh();
+    const handleinputchange = (e, index) => {
+        const {name, value} = e.target;
+        const list = [...inputList];
+        list[index][name]=value;
+        setinputList(list);
+        console.log(inputList)
+    }
+    const handleCreateFlashCardSet = async(event) => {
+        event.preventDefault();
 
-    console.log(flashcardInfo);
-  };
-  const handlerefresh = async () => {
-    console.log(getCookie("classCode"));
-    let res = await axios.post("http://localhost:3001/class", {
-      classCode: getCookie("classCode"),
-    });
-    console.log(res.data);
-    setLibrary(res.data);
-    localStorage.setItem("class", JSON.stringify(res.data));
-  };
-  const handleClose = () => {
-    setShow(false);
-    setinputList([{ front: "", back: "", drate: "3", img: "" }]);
-  };
-  const handleShow = () => setShow(true);
-  const handleimage = (e, i) => {
-    const { name } = e.target;
-    const list = [...inputList];
-    fileReader.onload = (r) => {
-      list[i][name] = r.target.result;
-    };
-    fileReader.readAsDataURL(e.target.files[0]);
-    setinputList(list);
-  };
-  {
-    /*Student list handlers */
-  }
-  const [showStudentList, setShowStudentList] = useState(false);
-  const handleRemoveStudent = async (name) => {
-    console.log(name);
-    let res = await axios.post("http://localhost:3001/leaveClassbyName", {
-      userName: name,
-      classCode: getCookie("classCode"),
-    });
-    handlerefresh();
-    if (res.data === true) {
-      alert("Student has been kicked");
+        const flashcardInfo = {
+            inputList:inputList,
+            name:name,
+            classCode:getCookie('classCode')
+        }
+        console.log(flashcardInfo);
+        let res = await axios.post("http://localhost:3001/createTeacherSet", {
+            inputList:flashcardInfo.inputList,
+            name:flashcardInfo.name,
+            classCode:flashcardInfo.classCode,
+        });
+
+        if(res.data===true){
+            handleShowSaved();
+        }
+        handleClose();
+        handlerefresh();
+
+        console.log(flashcardInfo);
     }
-  };
-  const handleDeleteClass = async () => {
-    console.log(name);
-    let res = await axios.post("http://localhost:3001/deleteClass", {
-      classCode: getCookie("classCode"),
-    });
-    navigate("/TeacherHomePage");
-    if (res.data === true) {
-      alert("Class Deleted");
+    const handlerefresh = async () => {     
+        console.log(getCookie('classCode'));
+        let res = await axios.post("http://localhost:3001/class", {
+            classCode:getCookie('classCode')
+        });
+        console.log(res.data);
+        setLibrary(res.data);
+        localStorage.setItem('class', JSON.stringify(res.data));
     }
-    const [showFolderDeleteConfirm, setShowFolderDeleteConfirm] = useState(false);
-    const handleCloseFolderDeleteConfirm = () => {setShowFolderDeleteConfirm(false);}
+    const handleClose = () => {
+        setShow(false);
+        setinputList([{front:'', back:'', drate:'3', img:''}]);
+    }
+    const handleShow = () => setShow(true); 
+    const handleimage = (e, i) => {
+        const {name} = e.target;
+        const list = [...inputList];
+        fileReader.onload = r => {
+            list[i][name]=r.target.result;
+        };
+        fileReader.readAsDataURL(e.target.files[0]);
+        setinputList(list);
+    }
+    {/*Student list handlers */}
+    const [showStudentList, setShowStudentList] = useState(false);
+    const handleRemoveStudent = async(name) => {
+        console.log(name)
+        let res = await axios.post("http://localhost:3001/leaveClassbyName", {
+            userName: name,
+            classCode:getCookie('classCode')
+        });
+        handlerefresh();
+        if (res.data === true) {
+            alert("Student has been kicked");
+        }
+    }
+    const handleDeleteClass = async () => {
+        console.log(name)
+        let res = await axios.post("http://localhost:3001/deleteClass", {
+            classCode:getCookie('classCode')
+        });
+        navigate('/TeacherHomePage');
+        if (res.data === true) {
+            alert("Class Deleted");
+        }
+    }
     return (
         <div>
             <TeacherHeader/>
@@ -178,7 +161,7 @@ function TeacherClass() {
                     <Button variant='dark' style={{color: 'gold'}} onClick={(e) => setShow(true)}>Create FlashCard Set</Button>
                     </div>
                     <div >
-                        <Button variant='danger' onClick={() => setShowFolderDeleteConfirm(true)}>Delete Class</Button>
+                        <Button variant='danger' onClick={() => handleDeleteClass()}>Delete Class</Button>
                     </div>
                 </div>
                 &nbsp;&nbsp;&nbsp;&nbsp;
@@ -199,7 +182,7 @@ function TeacherClass() {
                     })}
                 </div>
             </div>
-            <Modal show={show} onHide={handleClose} backdrop="static" dialogClassName="general-box-createflash">
+            <Modal show={show} onHide={handleClose} dialogClassName="general-box-createflash">
                 <Modal.Header style={{backgroundColor: 'black', color: 'gold'}}>
                     <Modal.Title> 
                         <h1 style ={{fontSize: "5rem", textAlign: "center"}}>BOILERCARDS</h1>
@@ -221,59 +204,45 @@ function TeacherClass() {
                                     <textarea type="text" name= "front" placeholder="Front of FlashCard" onChange={e => handleinputchange(e,i)}/>
                                 </div>
                             </Form.Group>
-                <Form.Group style={{ color: "gold" }}>
-                  <Form.Label>Back of Card</Form.Label>
-                  <div>
-                    <textarea
-                      type="text"
-                      name="back"
-                      placeholder="Back of FlashCard"
-                      onChange={(e) => handleinputchange(e, i)}
-                    />
-                  </div>
-                </Form.Group>
-                <input
-                  type="file"
-                  name="img"
-                  accept="image/png"
-                  onChange={(e) => handleimage(e, i)}
-                />
-                <Form.Group style={{ color: "gold" }}>
-                  <Form.Label>Difficulty Rating</Form.Label>
-                  <select
-                    name="drate"
-                    id="Difficulty-Rating"
-                    onChange={(e) => handleinputchange(e, i)}
-                  >
-                    <option value={1}>1</option>
-                    <option value={2}>2</option>
-                    <option selected value={3}>
-                      3
-                    </option>
-                    <option value={4}>4</option>
-                    <option value={5}>5</option>
-                  </select>
-                </Form.Group>
-              </Form>
-            );
-          })}
-          <div style={{ paddingTop: "1rem" }}>
-            <Button varient="primary" type="button" onClick={handleaddmore}>
-              Add FlashCard
-            </Button>
-          </div>
-        </Modal.Body>
-        <Modal.Footer style={{ backgroundColor: "black" }}>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleCreateFlashCardSet}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
 
-      {/*Student List*/}
+                            <Form.Group style={{color: "gold"}}>
+                                <Form.Label>Back of Card</Form.Label>
+                                <div>
+                                    <textarea type="text" name= "back" placeholder="Back of FlashCard" onChange={e => handleinputchange(e,i)} />
+                                </div>
+                            </Form.Group>
+                            <input type='file' name='img' accept="image/png" onChange={(e) => handleimage(e,i)}/>
+                            <Form.Group style={{color: "gold"}}>
+                            <Form.Label>Difficulty Rating</Form.Label>
+                                <select name ="drate" id="Difficulty-Rating" onChange={(e) => handleinputchange(e,i)}>
+                                    <option value={1}>1</option>
+                                    <option value={2}>2</option>
+                                    <option selected value={3}>3</option>
+                                    <option value={4}>4</option>
+                                    <option value={5}>5</option>
+                                </select>
+                            </Form.Group>
+                        </Form>
+                    );
+                    })}
+                    <div style={{paddingTop: "1rem"}}>
+                        <Button varient= "primary" type="button" onClick={handleaddmore}>
+                            Add FlashCard
+                        </Button>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer style={{backgroundColor: 'black'}}>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleCreateFlashCardSet}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            {/*Student List*/}
+
             <Modal show={showStudentList} onHide={() => setShowStudentList(false)}>
                 <Modal.Header style={{backgroundColor: 'black', color: 'gold'}}>
                     <Modal.Title>Student List</Modal.Title>
@@ -306,39 +275,18 @@ function TeacherClass() {
             </Modal>
             {/** Saved Modal **/}
             <Modal show={showSaved} onHide={() => handleCloseSaved()}>
-                <Modal.Header closeButton={() => handleCloseSaved()} style={{backgroundColor: 'black', color: 'gold'}}>
+                <Modal.Header closeButton={() => handleCloseSaved()}>
                     <Modal.Title> Successful Operation</Modal.Title>
                 </Modal.Header>
-                <Modal.Body style={{backgroundColor: 'dimgrey', color: 'gold'}}> 
+                <Modal.Body> 
                         <img className="photo" src= {saveicon}/>
                 </Modal.Body>
-                <Modal.Footer style={{backgroundColor: 'black', color: 'gold'}}>
+                <Modal.Footer>
                     <Button onClick={() => handleCloseSaved()}> Acknowledge </Button>
                 </Modal.Footer>
             </Modal>
-            {/* Delete Confirm Modal */}
-            <Modal show={showFolderDeleteConfirm} onHide={() => handleCloseFolderDeleteConfirm()}>
-                <Modal.Header closeButton={() => handleCloseFolderDeleteConfirm()} style={{backgroundColor: 'black', color: 'gold'}}>
-                    <Modal.Title>Delete Confirmation</Modal.Title>
-                </Modal.Header>
-                <Modal.Body style={{backgroundColor: 'dimgrey', color: 'gold'}}> Are you sure you want to delete {library.className}?</Modal.Body>
-                <Modal.Footer style={{backgroundColor: 'black', color: 'gold'}}>
-                    <Button onClick={() => handleDeleteClass()}> Delete </Button>
-                    <Button onClick={() => handleCloseFolderDeleteConfirm()}> Cancel </Button>
-                </Modal.Footer>
-            </Modal>
-            <Modal show={showFolderDeleteConfirm} onHide={() => handleCloseFolderDeleteConfirm()}>
-                <Modal.Header closeButton={() => handleCloseFolderDeleteConfirm()} style={{backgroundColor: 'black', color: 'gold'}}>
-                    <Modal.Title>Kick Student</Modal.Title>
-                </Modal.Header>
-                <Modal.Body style={{backgroundColor: 'dimgrey', color: 'gold'}}> Are you sure you want to Kick {library.className}?</Modal.Body>
-                <Modal.Footer style={{backgroundColor: 'black', color: 'gold'}}>
-                    <Button onClick={() => handleDeleteClass()}> Delete </Button>
-                    <Button onClick={() => handleCloseFolderDeleteConfirm()}> Cancel </Button>
-                </Modal.Footer>
-            </Modal>
     </div>
-  );
+    );
 }
 
 export default TeacherClass;
